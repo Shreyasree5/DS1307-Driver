0a5ca2a954fa1f4945b2cab262368357675fd8d0
Ravutla Shreyasree <ravutlashreyasree05@gmail.com>
2025-01-14 19:16:36 +0530

    Working 12 & 24hrs mode

--- myds1307.py
+++ myds1307.py
@@ -14,11 +14,12 @@ def validate(date_time):
         return False, "Seconds must be in range 0-59"
     if not (0 <= date_time["minutes"] <= 59):
         return False, "Minutes must be in range 0-59"
-    if (date_time["AMPM"]=="AM" or date_time["AMPM"]=="PM"):
-        if not(date_time["hours"]>=0 and date_time["hours"]<=12):
-            return False, "Hours must be in range 0-12"  
-    if not (1 <= date_time["day"] <= 7):
-        return False, "Day must be in range 1-7 (Monday-Sunday)"
+    if ('AMPM' in date_time.keys()):
+        if (date_time["AMPM"]=="AM" or date_time["AMPM"]=="PM"):
+            if not(date_time["hours"]>=0 and date_time["hours"]<=12):
+                return False, "Hours must be in range 0-12"  
+        if not (1 <= date_time["day"] <= 7):
+            return False, "Day must be in range 1-7 (Monday-Sunday)"
     if not (1 <= date_time["date"] <= 31):
         return False, "Date must be in range 1-31"
     if not (1 <= date_time["month"] <= 12):
@@ -41,7 +42,18 @@ def setTime(date_time):
         is_valid, message = validate(date_time_to_set)
         if is_valid:
             print(message)
-            date_time_bcd = [decimal_to_bcd(date_time["seconds"]), decimal_to_bcd(date_time["minutes"]), decimal_to_bcd(date_time["hours"]), decimal_to_bcd(date_time["day"]), decimal_to_bcd(date_time["date"]), decimal_to_bcd(date_time["month"]), decimal_to_bcd(date_time["year"])]
+            n = decimal_to_bcd(date_time["hours"])
+            
+            if ('AMPM' in date_time.keys()):
+                n = n | (1<<6)
+                if (date_time["AMPM"]=='PM'):
+                    n = (1<<5)|n
+                else:
+                    n = ~(1<<5)&n
+            else:
+                n = n & ~(1<<6)
+
+            date_time_bcd = [decimal_to_bcd(date_time["seconds"]), decimal_to_bcd(date_time["minutes"]), n, decimal_to_bcd(date_time["day"]), decimal_to_bcd(date_time["date"]), decimal_to_bcd(date_time["month"]), decimal_to_bcd(date_time["year"])]
             bus.write_i2c_block_data(DS1307_ADDRESS, start_register, date_time_bcd)
             print("Date and Time set succussfully:")
             print(date_time)
@@ -52,17 +64,35 @@ def setTime(date_time):
 
 def getTime():
 
-     data_time = bus.read_i2c_block_data(DS1307_ADDRESS, start_register, 8)
+    data_time = bus.read_i2c_block_data(DS1307_ADDRESS, start_register, 7)
+    n =((data_time[2] & (1<<6))>>6)
+    if ( n != 0):
+        if ((1<<5)& data_time[2] != 0):
+            ampm = 'PM'
+        else:
+            ampm = 'AM'
 
-     return {
-        "seconds": bcd_to_decimal(data_time[0]),
-        "minutes":bcd_to_decimal(data_time[1]),
-        "hours":bcd_to_decimal(data_time[2]),
-        "day":bcd_to_decimal(data_time[3]),
-        "date":bcd_to_decimal(data_time[4]),
-        "month":bcd_to_decimal(data_time[5]),
-        "year":bcd_to_decimal(data_time[6])
-     }
+        ret = {
+            "seconds": bcd_to_decimal(data_time[0]),
+            "minutes":bcd_to_decimal(data_time[1]),
+            "hours":bcd_to_decimal(data_time[2]&0x1F),
+            "day":bcd_to_decimal(data_time[3]),
+            "date":bcd_to_decimal(data_time[4]),
+            "month":bcd_to_decimal(data_time[5]),
+            "year":bcd_to_decimal(data_time[6]),
+            "AMPM":ampm
+        }
+        return ret
+    else:
+        return {
+            "seconds": bcd_to_decimal(data_time[0]),
+            "minutes":bcd_to_decimal(data_time[1]),
+            "hours":bcd_to_decimal(data_time[2]&0x1F),
+            "day":bcd_to_decimal(data_time[3]),
+            "date":bcd_to_decimal(data_time[4]),
+            "month":bcd_to_decimal(data_time[5]),
+            "year":bcd_to_decimal(data_time[6]),
+        }
 
 #input date and time to ds1307
 date_time_to_set = {
@@ -73,7 +103,7 @@ date_time_to_set = {
        "date":5,
        "month":9,
        "year":99,
-       "AMPM": "PM"
+       #"AMPM": "AM"
 }
 
 setTime(date_time_to_set)
